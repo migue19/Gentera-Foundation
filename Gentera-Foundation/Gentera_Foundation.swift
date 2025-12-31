3d24503e8da76792fe18d027bbd615a633d7b8ef
Miguel Mexicano Herrera <e-mmexicano@compartamos.com>
2025-12-31 10:00:37 -0600

    Initial Commit

---
+++ Gentera-Foundation/Gentera_Foundation.swift
@@ -0,0 +1,9 @@
+//
+//  Gentera_Foundation.swift
+//  Gentera-Foundation
+//
+//  Created by Miguel Mexicano Herrera on 31/12/25.
+//
+
+import Foundation
+
---
+++ Gentera-FoundationTests/Gentera_FoundationTests.swift
@@ -0,0 +1,17 @@
+//
+//  Gentera_FoundationTests.swift
+//  Gentera-FoundationTests
+//
+//  Created by Miguel Mexicano Herrera on 31/12/25.
+//
+
+import Testing
+@testable import Gentera_Foundation
+
+struct Gentera_FoundationTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
